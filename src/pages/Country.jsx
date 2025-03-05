678040df471e8ebbbcd00df0273e6746b4ea6336
Shubhan <shubhambodalkae@gmail.com>
2025-03-06 03:18:35 +0530

    Fetch country data from api

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState, useTransition } from 'react'
+import "./loading.css";
+import { getData } from '../api/CountriesData';
 
 const Country = () => {
+  const [isPending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() =>{
+    startTransition(async () => {
+      const res = await getData();
+      console.log(res.data);
+      
+      setCountries(res.data);
+    })
+  },[])
+
+  if (isPending) {
+    return <span className="loader">L &nbsp; ading</span>
+  }
+
   return (
     <div>
       <h1>This is Country page.</h1>
