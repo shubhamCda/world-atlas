385bf1f8cba7c154d031da15641ff9c1cc22b790
Shubhan <shubhambodalkae@gmail.com>
2025-03-05 10:09:16 +0530

    Add error page

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,34 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Country from './pages/Country'
 import Contact from './pages/Contact'
+import AppLayout from './components/layout/AppLayout'
+import ErrorPage from './pages/ErrorPage'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
-  },
-  {
-    path: "about",
-    element: <About />
-  },
-  {
-    path: "country",
-    element: <Country />
-  },
-  {
-    path: "contact",
-    element: <Contact />
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "country",
+        element: <Country />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      }
+    ]
   }
+  
 ])
 
 const App = () => {
