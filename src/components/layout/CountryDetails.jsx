896c4f6b48afd8ba0b680b4c53edd591ce099570
Shubhan <shubhambodalkae@gmail.com>
2025-03-06 09:02:52 +0530

    Create country detail component

--- src/components/layout/CountryDetails.jsx
+++ src/components/layout/CountryDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useTransition } from "react";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import { getCountryData } from "../../api/CountriesData";
 import "../../pages/loading.css";
 
@@ -17,8 +17,7 @@ const CountryDetails = () => {
       if (res.status === 200) {
         setCountry(res.data[0]);
       }
-      console.log(country);
-
+      // console.log(country);
     });
   }, []);
 
@@ -31,13 +30,73 @@ const CountryDetails = () => {
       </>
     );
   }
-  console.log(params);
+  if (country !== undefined) {
+    return (
+      <section className="card country-details-card container">
+      <div className="container-card bg-white-box">
+        {country && (
+          <div className="country-image grid grid-two-cols">
+            <img
+              src={country.flags.svg}
+              alt={country.flags.alt}
+              className="flag"
+            />
+            <div className="country-content">
+              <p className="card-title"> {country.name.official} </p>
 
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  );
+              <div className="infoContainer">
+                <p>
+                  <span className="card-description"> Native Names:</span>
+                  {Object.keys(country.name.nativeName)
+                    .map((key) => country.name.nativeName[key].common)
+                    .join(", ")}
+                </p>
+                <p>
+                  <span className="card-description"> Population: </span>
+                  {country.population.toLocaleString()}
+                </p>
+                <p>
+                  <span className="card-description"> Region:</span>
+                  {country.region}
+                </p>
+                <p>
+                  <span className="card-description"> Sub Region:</span>
+                  {country.subregion}
+                </p>
+                <p>
+                  <span className="card-description"> Capital:</span>
+                  {country.capital}
+                </p>
+
+                <p>
+                  <span className="card-description">Top Level Domain:</span>
+                  {country.tld[0]}
+                </p>
+                <p>
+                  <span className="card-description"> Currencies: </span>
+                  {Object.keys(country.currencies)
+                    .map((curElem) => country.currencies[curElem].name)
+                    .join(", ")}
+                </p>
+                <p>
+                  <span className="card-description">Languages: </span>
+                  {Object.keys(country.languages)
+                    .map((key) => country.languages[key])
+                    .join(", ")}
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="country-card-backBtn">
+          <NavLink to="/country" className="backBtn">
+            <button>Go Back</button>
+          </NavLink>
+        </div>
+      </div>
+    </section>
+    );
+  }
 };
 
 export default CountryDetails;
